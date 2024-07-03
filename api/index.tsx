6f63302d30cd993ed07ec253563c8151ab7bbe5b
chris <ngmi@posteo.es>
2024-07-03 14:21:48 +0200

    updates

---
+++ api/index.tsx
@@ -0,0 +1,57 @@
+import { Button, Frog } from "frog";
+import { devtools } from "frog/dev";
+import { serveStatic } from "frog/serve-static";
+import { handle } from "frog/vercel";
+
+export const app = new Frog({
+  assetsPath: "/",
+  basePath: "/api",
+});
+
+app.frame("/", (c) => {
+  return c.res({
+    image: (
+      <div style={{ color: "white", display: "flex", fontSize: 60 }}>
+        Add "Herocast0r" Action
+      </div>
+    ),
+    intents: [
+      <Button.AddCastAction action="/log-this">Add</Button.AddCastAction>,
+    ],
+  });
+});
+
+app.castAction(
+  "/log-this",
+  async (c) => {
+    const { actionData } = c;
+    const { messageHash } = actionData;
+
+    if (!messageHash) {
+      return c.res({
+        message: "Message hash not found",
+        type: "message",
+      });
+    }
+
+    const herocastUrl = `https://app.herocast.xyz/conversation/${messageHash}`;
+    console.log(
+      `Redirecting to ${herocastUrl} for castId ${actionData.castId} from ${actionData.fid}`
+    );
+
+    return c.res({
+      message: "Redirecting to HeroCast",
+      link: herocastUrl,
+      type: "message",
+    });
+  },
+  { name: "Log This!", icon: "log" }
+);
+
+// @ts-ignore
+const isEdgeFunction = typeof EdgeFunction !== "undefined";
+const isProduction = isEdgeFunction || import.meta.env?.MODE !== "development";
+devtools(app, isProduction ? { assetsPath: "/.frog" } : { serveStatic });
+
+export const GET = handle(app);
+export const POST = handle(app);
